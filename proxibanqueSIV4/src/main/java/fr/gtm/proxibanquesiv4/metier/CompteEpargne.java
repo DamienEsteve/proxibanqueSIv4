5de7a90cade41bacb7bcc9b25444d76c2e489eb0
Guillaume Jamin <guillaumejamin42@gmail.com>
2017-01-24 13:45:43 +0100

    oops, petit oubli

--- proxibanqueSIV4/src/main/java/fr/gtm/proxibanquesiv4/metier/CompteEpargne.java
+++ proxibanqueSIV4/src/main/java/fr/gtm/proxibanquesiv4/metier/CompteEpargne.java
@@ -5,7 +5,6 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
-import javax.persistence.Table;
 import javax.persistence.Transient;
 
 import org.apache.log4j.Logger;
