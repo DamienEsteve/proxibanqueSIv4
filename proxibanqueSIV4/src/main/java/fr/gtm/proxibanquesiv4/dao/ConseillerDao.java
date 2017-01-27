b70729616f89cb8437ab93b91fd486197f91dcfa
Guillaume Jamin <guillaumejamin42@gmail.com>
2017-01-27 13:42:40 +0100

    Un peu de javadoc ?

--- proxibanqueSIV4/src/main/java/fr/gtm/proxibanquesiv4/dao/ConseillerDao.java
+++ proxibanqueSIV4/src/main/java/fr/gtm/proxibanquesiv4/dao/ConseillerDao.java
@@ -15,32 +15,53 @@ import fr.gtm.proxibanquesiv4.metier.Compte;
 import fr.gtm.proxibanquesiv4.metier.Conseiller;
 import fr.gtm.proxibanquesiv4.metier.Virement;
 
+/**
+ * @author Guillaume, Severine, Kevin, Damien
+ *
+ */
 @Repository("conseillerDao")
 @Transactional
 public class ConseillerDao implements IConseillerDao {
 
+	
 	@Autowired
 	private SessionFactory sessionFactory;
+	/**
+	 * @return return the session for JPA
+	 */
 	private Session getSession(){
 		return sessionFactory.getCurrentSession();
 	}
 
+	/**
+	 * Methode de creation d'un client
+	 */
 	@Override
 	public void createClient(Client cl) {
 		getSession().save(cl);
 	}
 
+	/**
+	 * Methode de mise à jour d'un client
+	 */
 	@Override
 	public void updateClient(Client cl) {
 		System.out.println(cl);
 		getSession().update(cl);
 	}
 
+	
+	/**
+	 * Methode de mise à jour d'un compte
+	 */
 	@Override
 	public void updateCompte(Compte cpt) {
 		getSession().update(cpt);
 	}
 
+	/**
+	 * Methode de selection d'un conseiller a partir de son login
+	 */
 	@Override
 	public Conseiller selectConseillerByLogin(String l) {
 		Query query = getSession().createQuery("Select c from Conseiller c where c.login =:l");
@@ -48,6 +69,9 @@ public class ConseillerDao implements IConseillerDao {
 		return (Conseiller) query.getSingleResult();
 	}
 
+	/**
+	 * Methode de selection des comptes d'un client
+	 */
 	@Override
 	public List<Compte> selectComptesByClientId(long idClient) {
 		Query query = getSession().createQuery("Select cpt from Compte cpt where cpt.client.idPersonne =:idClient");
@@ -55,6 +79,9 @@ public class ConseillerDao implements IConseillerDao {
 		return query.getResultList();
 	}
 
+	/**
+	 * Methode de selection des clients d'un conseiller
+	 */
 	@Override
 	public List<Client> selectClientsByConsId(long idCons) {
 		Query query = getSession().createQuery("Select cl from Client cl where cl.conseiller.idPersonne =:idCons");
@@ -62,18 +89,27 @@ public class ConseillerDao implements IConseillerDao {
 		return query.getResultList();
 	}
 
+	/**
+	 * Methode de selection de tout les comptes
+	 */
 	@Override
 	public List<Compte> selectAllComptes() {
 		Query query = getSession().createQuery("Select cpt from Compte cpt");
 		return query.getResultList();
 	}
 
+	/**
+	 * Methode de selection de tout les clients
+	 */
 	@Override
 	public List<Client> selectAllClients() {
 		Query query = getSession().createQuery("Select cl from Client cl");
 		return query.getResultList();
 	}
 
+	/**
+	 * Methode de creation d'un virement
+	 */
 	@Override
 	public void createVirementDao(Virement vir) {
 		Compte cc = vir.getCompteCrediteur();
@@ -85,17 +121,26 @@ public class ConseillerDao implements IConseillerDao {
 		getSession().save(vir);
 	}
 	
+	/**
+	 * Methode de creation de compte
+	 */
 	@Override
 	public void createCompte(Compte c) {
 		getSession().save(c);
 	}
 	
+	/**
+	 * Methode de selection d'un compte
+	 */
 	@Override
 	public Compte selectCompteById(long idCpt) {
 		Compte c = (Compte) getSession().get(Compte.class, idCpt);
 		return c;
 	}
 	
+	/**
+	 * Methode de selection de l'id d'un conseiller grace à son login
+	 */
 	@Override
 	public long SelectIdByLoginConseiller(String l){
 		Query query = getSession().createQuery("Select c.idPersonne from Conseiller c where c.login =:l");
